e10fbaf6d1be3c7541a9338bb14cd97855f18015
Koson Trachu <koson.trachu@gmail.com>
2016-10-26 11:24:31 +0700

    Update eventHandler.cs

--- eventHandler.cs
+++ eventHandler.cs
@@ -1,31 +1,39 @@
 using System;
 
-delegate void Handler();
-class Incrementer
+delegate void Handler(); // 1 ประกาศ delegate
+
+class Incrementer // publisher
 {
-    public event Handler CountADozen;
+    public event Handler CountADozen; // 2  ประกาศ event
+    // event CountADozen อยู่โดดเดี่ยวไม่ได้ จะต้องใส่ชื่อของ delegate ด้วย กลายเป็น
+    // event Handler CountADozen แต่ยังมองไม่เห็นจากภายนอก ต้องประกาศเป็น public
+    // public event Handler CountADozen
+
     const int length = 100;
-    public void DoCount()
+    public void DoCount()  // สร้างสถานการณ์ เพื่อที่จะ firing event
     {
         for (int i = 1; i < length; i++)
         {
-            if (i%12 == 0 && CountADozen != null)
+            if (i % 12 == 0 && CountADozen != null)
             {
-                CountADozen();
+                CountADozen(); // firing event เมื่อหารด้วย 12 ลงตัว
             }
-        }    
+        }
     }
 }
 
-class Dozens
+class Dozens  // subscriber
 {
-    public int DozenCount { get; private set; }
-    public Dozens(Incrementer incrementer)
-    { DozenCount = 0;
-        incrementer.CountADozen += IncrementerDozenCount;
+    public int DozenCount { get; private set; }  // property 
+
+    public Dozens(Incrementer incrementer) // ลงทะเบียนเหตุการณ์
+    {
+        DozenCount = 0;
+        incrementer.CountADozen += this.IncrementerDozenCount;
+        // ผู้ส่ง.เหตุการณ์ เชื่อมกับ ผู้รับ.เมธอด  
     }
 
-    private void IncrementerDozenCount()
+    private void IncrementerDozenCount()  // event handler
     {
         DozenCount++;
     }
@@ -35,9 +43,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Incrementer incrementer = new Incrementer();
-        Dozens dozenCounter = new Dozens(incrementer);
-        incrementer.DoCount();
-        Console.WriteLine("number of dozen = {0}", dozenCounter.DozenCount);
+        Incrementer incrementer = new Incrementer();  // สร้าง publisher
+        Dozens dozenCounter = new Dozens(incrementer); // สร้าง subscriber โดยระบุ publisher เป็น parameter 
+        incrementer.DoCount(); // จำลองสถานการณ์ที่ firing event
+        Console.WriteLine("number of dozen = {0}", dozenCounter.DozenCount); // รายงานสรุป
     }
 }
